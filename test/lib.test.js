9fd1b480eb5913ba084fb95b830b911a0c3c85a7
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-30 14:11:00 +0800

    test exceptions

--- test/lib.test.js
+++ test/lib.test.js
@@ -54,11 +54,29 @@ describe('getCurrencies', () => {
 });
 
 describe('getProduct', () => {
-  it('should return the product with the given id', () => {
-    const result = lib.getProduct(1);
+	it('should return the product with the given id', () => {
+		const result = lib.getProduct(1);
 
-    expect(result).toEqual({ id: 1, price: 10 })
-    expect(result).toMatchObject({ id: 1, price: 10 })
-    expect(result).toHaveProperty('id', 1);
-  })
-})
+		expect(result).toEqual({ id: 1, price: 10 });
+		expect(result).toMatchObject({ id: 1, price: 10 });
+		expect(result).toHaveProperty('id', 1);
+	});
+});
+
+describe('registerUser', () => {
+	it('should throw if username is falsy', () => {
+		const args = [null, undefined, NaN, '', 0, false];
+		args.forEach((a) => {
+			expect(() => {
+				lib.registerUser(a);
+			}).toThrow();
+		});
+	});
+
+	it('should return a user object if valid username is passed', () => {
+		const result = lib.registerUser('LD');
+
+		expect(result).toMatchObject({ username: 'LD' });
+		expect(result).toHaveProperty('id', result.id, 'username', 'LD');
+	});
+});
