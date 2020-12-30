0520630c4648c26efcd770d8140841573a1144f8
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-30 13:28:00 +0800

    Test arrays

--- test/lib.test.js
+++ test/lib.test.js
@@ -21,10 +21,34 @@ describe('absoute', () => {
 });
 
 describe('greet', () => {
-	it('shoult return the greeting message', () => {
+	it('should return the greeting message', () => {
 		const result = lib.greet('LD');
 
 		expect(result).toMatch(/LD/);
 		expect(result).toContain('LD');
+	});
+});
+
+describe('getCurrencies', () => {
+	it('should return supported currencies', () => {
+		const result = lib.getCurrencies();
+
+		// Too general
+		expect(result).toBeDefined();
+		expect(result).not.toBeNull();
+
+		// Too specific
+		expect(result[0]).toBe('USD');
+		expect(result[1]).toBe('AUD');
+		expect(result[2]).toBe('EUR');
+		expect(result.length).toBe(3);
+
+		// Proper way
+		expect(result).toContain('USD');
+		expect(result).toContain('AUD');
+		expect(result).toContain('EUR');
+
+		// Ideal way
+		expect(result).toEqual(expect.arrayContaining(['AUD', 'EUR', 'USD']));
 	});
 });
