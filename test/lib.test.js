b3c34db4fde60bc8c1a9a3f31f48246e8d0b458e
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-30 13:34:38 +0800

    Test object

--- test/lib.test.js
+++ test/lib.test.js
@@ -52,3 +52,13 @@ describe('getCurrencies', () => {
 		expect(result).toEqual(expect.arrayContaining(['AUD', 'EUR', 'USD']));
 	});
 });
+
+describe('getProduct', () => {
+  it('should return the product with the given id', () => {
+    const result = lib.getProduct(1);
+
+    expect(result).toEqual({ id: 1, price: 10 })
+    expect(result).toMatchObject({ id: 1, price: 10 })
+    expect(result).toHaveProperty('id', 1);
+  })
+})
