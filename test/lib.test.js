211b29c6c16f559e91d0d8ff9d78a4981b71263c
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-30 12:49:59 +0800

    test numbers

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,3 +1,19 @@
-test("initial test", () => {
-	
+const lib = require('../lib');
+
+test('absolute - should return a positive number if input is positive', () => {
+	const result = lib.absolute(1);
+
+	expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+	const result = lib.absolute(-1);
+
+	expect(result).toBe(1);
+});
+
+test('absolute - should return a 0 number if input is 0', () => {
+	const result = lib.absolute(0);
+
+	expect(result).toBe(0);
 });
